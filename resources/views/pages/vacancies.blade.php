040c701c756821e68bfd3a467e3e9a5a1c336d13
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-09-15 19:21:03 +0200

    titles

--- resources/views/pages/vacancies.blade.php
+++ resources/views/pages/vacancies.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
 
     <x-slot name="title">
-        Contact
+        Vacancies
     </x-slot>
 
     <!-- Banner Section -->
--- resources/views/pages/vacancy.blade.php
+++ resources/views/pages/vacancy.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
 
     <x-slot name="title">
-        Contact
+        Vacancies - {{ $vacancy->title }}
     </x-slot>
 
     <!-- Banner Section -->
